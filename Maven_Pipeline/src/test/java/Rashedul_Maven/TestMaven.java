ab7600aaf407039dadfccfd599e7be8b3e2dbc45
Rashedul Bari <TN302BM@tn.gov>
2021-03-23 09:33:47 -0500

    Pushing Maven Project from eclipse

---
+++ Maven_Pipeline/src/test/java/Rashedul_Maven/TestMaven.java
@@ -0,0 +1,16 @@
+package Rashedul_Maven;
+
+import org.testng.annotations.Test;
+
+public class TestMaven {
+	
+	@Test
+	public void testMaven() {
+		
+	System.out.println("Running Maven project");
+	
+	//Command to run testng test from command prompt.
+     //mvn clean test -DtestingFile=testng.xml
+	}
+
+}
